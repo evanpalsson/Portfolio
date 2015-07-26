8c8bea1395f75cd5f0f3cc60dea0355dafcedb6b
evanpalsson <evanpalsson@gmail.com>
2015-07-25 23:45:07 -0600

    saving work before bed

--- public/app.js
+++ public/app.js
@@ -2,24 +2,23 @@ var app = angular.module('portfolio', ['ngRoute']);
 
 	app.config(function($routeProvider){
 
-	$routeProvider
-	  	.when('/', {
-	  		templateUrl: './main.html'
-	  		// controller:	'mainCtrl'
-	  	})
-	  	.when('/svg', {
-	  		templateUrl: './svg.html',
-	  		controller: 'svgCtrl'	
-	  	})
-	  	.when('/chart', {
-	  		templateUrl: './chart.html'
-	  		// controller: 'chartCtrl'	
-	  	})
-	  	.otherwise({
-	  		redirectTo: '/'
-	  	})
-
-
-
-
+		$routeProvider
+		  	.when('/', {
+		  		templateUrl: './main.html'
+		  		// controller:	'mainCtrl'
+		  	})
+		  	.when('/svg', {
+		  		templateUrl: './svg.html',
+		  		controller: 'svgCtrl'	
+		  	})
+		  	.when('/chart', {
+		  		templateUrl: './chart.html'
+		  		// controller: 'chartCtrl'	
+		  	})
+		  	.otherwise({
+		  		redirectTo: '/'
+		  	})
 	});
+
+
+
